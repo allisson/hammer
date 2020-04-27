5a15887b6057f8b991f784f0a8c3dd98a24f9edf
Allisson Azevedo <allisson@gmail.com>
2020-04-27 15:33:01 -0300

    Add SubscriptionService

--- service.go
+++ service.go
@@ -7,3 +7,11 @@ type TopicService interface {
 	Create(topic *Topic) error
 	Update(topic *Topic) error
 }
+
+// SubscriptionService interface
+type SubscriptionService interface {
+	Find(id string) (Subscription, error)
+	FindAll(limit, offset int) ([]Subscription, error)
+	Create(subscription *Subscription) error
+	Update(subscription *Subscription) error
+}
