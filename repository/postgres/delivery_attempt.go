8d3bf1b68b62d606470d1e46643f2431f80267b8
Allisson Azevedo <allisson@gmail.com>
2020-05-11 22:53:13 -0300

    Update sql statements with golang templates

--- repository/postgres/delivery_attempt.go
+++ repository/postgres/delivery_attempt.go
@@ -15,14 +15,32 @@ type DeliveryAttempt struct {
 // Find returns hammer.DeliveryAttempt by id
 func (d *DeliveryAttempt) Find(id string) (hammer.DeliveryAttempt, error) {
 	deliveryAttempt := hammer.DeliveryAttempt{}
-	err := d.db.Get(&deliveryAttempt, sqlDeliveryAttemptFind, id)
+	data := map[string]interface{}{
+		"table": "delivery_attempts",
+		"id":    id,
+	}
+	query, args, err := buildQuery(sqlFind, data)
+	if err != nil {
+		return deliveryAttempt, err
+	}
+	err = d.db.Get(&deliveryAttempt, query, args...)
 	return deliveryAttempt, err
 }
 
 // FindAll returns []hammer.DeliveryAttempt by limit and offset
 func (d *DeliveryAttempt) FindAll(limit, offset int) ([]hammer.DeliveryAttempt, error) {
 	deliveryAttempts := []hammer.DeliveryAttempt{}
-	err := d.db.Select(&deliveryAttempts, sqlDeliveryAttemptFindAll, limit, offset)
+	data := map[string]interface{}{
+		"table":   "delivery_attempts",
+		"limit":   limit,
+		"offset":  offset,
+		"orderBy": "id DESC",
+	}
+	query, args, err := buildQuery(sqlFindAll, data)
+	if err != nil {
+		return deliveryAttempts, err
+	}
+	err = d.db.Select(&deliveryAttempts, query, args...)
 	return deliveryAttempts, err
 }
 
